6fcadeae1675400a969a5f055bba4f4d02ac3915
Nicole de Guttry <nicole.j36.15@gmail.com>
2026-07-27 11:42:12 +0200

    Add initial job listings

---
+++ src/data/jobs.js
@@ -0,0 +1,28 @@
+const jobs = [
+  {
+    id: 1,
+    title: 'Junior Frontend Developer',
+    company: 'Tech Solutions',
+    location: 'Roma',
+    type: 'Full-time',
+    technologies: ['JavaScript', 'React', 'CSS'],
+  },
+  {
+    id: 2,
+    title: 'Junior PHP Developer',
+    company: 'Web Agency',
+    location: 'Milano',
+    type: 'Full-time',
+    technologies: ['PHP', 'Laravel', 'MySQL'],
+  },
+  {
+    id: 3,
+    title: 'Frontend Developer',
+    company: 'Digital Studio',
+    location: 'Remote',
+    type: 'Full-time',
+    technologies: ['React', 'JavaScript', 'HTML', 'CSS'],
+  },
+]
+
+export default jobs
